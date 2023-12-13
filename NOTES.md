0f9ac16c246f6bb6dac57afd790fb2c3017678e1
horologger <horologger@protonmail.com>
2023-12-13 15:00:15 -0500

    going back to master

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+$ registry-publish reg add -l https://start9registry.isviable.com/ -n IsViable -u alunde -p Plak8484
+Below is the hash to provide to the server operator for your admin credentials
+b1671104a27b4b5d2874f8edbb66ec926154a69f391b6d5ba8a7dcb4bd5ff764
+
--- setenv.sh
+++ setenv.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Run like this  .  ./setenv.sh to externalize the vars
 
-export PATH=/home/alunde/.cabal/bin:/home/alunde/.ghcup/bin:/home/alunde/.cargo/bin:/home/alunde/.local/bin:$PATH
+# export PATH=/home/alunde/start9-registry/dist:/home/alunde/.cabal/bin:/home/alunde/.ghcup/bin:/home/alunde/.cargo/bin:/home/alunde/.local/bin:$PATH
+export PATH=/home/alunde/.local/bin:/home/alunde/start9-registry/dist:/home/alunde/.cabal/bin:/home/alunde/.ghcup/bin:/home/alunde/.cargo/bin:$PATH
 export RESOURCES_PATH=/home/alunde/start9-registry/repository
 export SSL_PATH=/home/alunde/start9-registry/ssl
 export SSL_AUTO=false
 export REGISTRY_HOSTNAME=start9registry.isviable.com
 export YESOD_PORT=8443
+#export YESOD_PORT=443
 export TOR_PORT=447
 export STATIC_BIN=/home/alunde/.cargo/bin/
 export ERROR_LOG_ROOT=/home/alunde/start9-registry/logs
